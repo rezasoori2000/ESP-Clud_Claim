447e82f2e9477ec8f24d312157694a9debaeac6e
reza.souri@gmail.com <reza.souri@gmail.com>
2021-06-08 10:41:42 +1200

    state event works fine

--- src/pages/AdminSettings/AdminSettingsContainer.jsx
+++ src/pages/AdminSettings/AdminSettingsContainer.jsx
@@ -1,45 +1,130 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import store from '../../store';
-import get_API_AdminSettings, { update_AdminSettings_State } from '../../store/actions/adminSettingsAction';
+import React from 'react';
 import AdminSettingsPage from './AdminSettingsPage';
-
-
+import config from '../../config';
+import axios from 'axios';
+import { TitleSharp } from '@material-ui/icons';
 
 class AdminSettingsContainer extends React.Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            adminSettings: {},
+            user: {}
+        }
+
+    }
     componentWillMount() {
-        this.props.getAdminSettingsByAPI();
+        this.getAdminSettingsByAPI();
+        this.captureIPAddress();
 
     }
     handelPropertyChange = (event) => {
+        var adSettings = this.state.adminSettings;
 
-        this.props.updateAdminSettingsState(event);
+        let value = '';
+        if (event.target.type != undefined && event.target.type === 'checkbox')
+            value = event.target.checked;
+        else
+            value = event.target.value;
+
+
+        console.log(`${event.target.name}  ${value}`);
+        adSettings[event.target.name] = value;
+        this.setState({
+            ...this.state,
+            adSettings
+        });
     }
+    onSelectChange = (event) => {
+        var adSettings = this.state.adminSettings;
+        var newSelectedId = event.target.value;
+        let newValues = [];
+        adSettings[event.target.name].map((item) => {
+            if (item.Value == newSelectedId)
+                newValues.push({
+                    'Value': item.Value,
+                    'Text': item.Text,
+                    'Disabled':false,
+                    'Group':null,
+                    'Selected': true,
+                });
+            else
+                newValues.push({
+                    'Value': item.Value,
+                    'Text': item.Text,
+                    'Disabled':false,
+                    'Group':null,
+                    'Selected': false
+                });
+        });
+        adSettings[event.target.name] = newValues;
+        this.setState({
+            ...this.state,
+            adminSettings: adSettings
+
+        });
+    }
+    captureIPAddress=async()=> {
+        fetch('https://api.ipify.org?format=jsonp?callback=?', {
+          method: 'GET',
+          headers: {},
+        })
+        .then(res => {
+          return res.text()
+        }).then(ip => {
+            this.setState({
+                ...this.state,
+                ip
+            });
+        });
+      }
+    
+
+    getAdminSettingsByAPI = async () => {
+        var response = {};
+        try {
+            const res = await axios.get(`${config.apiUrl}adminSettings`);
+            response = JSON.parse(res.data);
+            this.setState({
+                ...this.state,
+                adminSettings: response
+            });
+        }
+        catch (err) {
+            alert(`Error in calling ESP API- ${err}`);
+        }
+    };
+
+    saveAdminSettingsByAPI = async () => {
+        var response = {};
+        var payload={
+            user:'',
+            adminSettings:this.state.adminSettings,
+        };
+        try {
+            const res = await axios.post(`${config.apiUrl}adminSettings`,this.state);
+            response = JSON.parse(res.data);
+            this.setState({
+                ...this.state,
+                adminSettings: response
+            });
+        }
+        catch (err) {
+            alert(`Error in calling ESP API- ${err}`);
+        }
+    };
 
     render(props) {
         return (
-            <AdminSettingsPage adminSettings={this.props.state_adminSettings || null} onPropertyChange={this.handelPropertyChange} />
+            <AdminSettingsPage adminSettings={this.state.adminSettings || null} onPropertyChange={this.handelPropertyChange} onSelectChange={this.onSelectChange} />
         )
 
 
 
     }
 };
-const mapStateToProps = state => ({
-    state_adminSettings: state.adminSettings
-});
 
-const mapDispatchToProps = dispatch => {
-    return {
-        getAdminSettingsByAPI: () => {
-            dispatch(get_API_AdminSettings());
-        },
-        updateAdminSettingsState: (event) => {
-            dispatch(update_AdminSettings_State(event));
-        }
-    }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AdminSettingsContainer);
+export default AdminSettingsContainer;
 
