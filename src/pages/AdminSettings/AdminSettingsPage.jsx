a297088668980ceaaa742dceeb6468fbd57d7031
reza.souri@gmail.com <reza.souri@gmail.com>
2021-06-08 16:06:34 +1200

    Setting save ok

--- src/pages/AdminSettings/AdminSettingsPage.jsx
+++ src/pages/AdminSettings/AdminSettingsPage.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -14,7 +13,11 @@ import { grey } from '@material-ui/core/colors'
 import SaveIcon from '@material-ui/icons/Save';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme: Theme) =>
+
+
+const AdminSettingsPage = (props) => {
+
+    const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             minWidth: 275,
@@ -38,11 +41,13 @@ const useStyles = makeStyles((theme: Theme) =>
         pos: {
             marginBottom: 12,
         },
-
+        saveBtn:{
+            marginTop:40,
+            
+        }
 
     }));
-
-const AdminSettingsPage = (props) => {
+    
 
     const classes = useStyles();
     const bull = <span className={classes.bullet}>•</span>;
@@ -51,7 +56,7 @@ const AdminSettingsPage = (props) => {
         <div >
             <Card >
 
-                <CardHeader title="Database Settings" subheader="ESP database connection settings " avatar={<Avatar aria-label="recipe" style={{ backgroundColor: grey[900] }}> D </Avatar>
+                <CardHeader title="Database Settings" subheader="ESP database connection settings " avatar={<Avatar aria-label="recipe" style={{ backgroundColor: 'blue' }}> D </Avatar>
                 } />
                 <CardContent></CardContent>
                 <CardContent>
@@ -128,41 +133,41 @@ const AdminSettingsPage = (props) => {
             <hr />
             <Card >
 
-                <CardHeader title="General Settings" subheader="Job Claim section settings " avatar={<Avatar aria-label="recipe" style={{ backgroundColor: grey[900] }}> G </Avatar>
+                <CardHeader title="General Settings" subheader="Job Claim section settings " avatar={<Avatar aria-label="recipe" style={{ backgroundColor:'blue' }}> G </Avatar>
                 } />
                 <CardContent>
                     <Grid container spacing={3}>
                         <Grid item lg={6}   >
-                            <ESPSelect disabled name='Factory' label='Default Factory' helptext='Name of default Factory' items={props.adminSettings.Factories}  onPropertyChange={props.onSelectChange} />
-                            <ESPCheckbox name='MultipleJobs' label='Enable Multiple Jobs' checked={props.adminSettings.MultipleJobs}  onPropertyChange={props.onPropertyChange} />
+                            <ESPSelect disabled name='Factories' label='Default Factory' helptext='Name of default Factory' items={props.adminSettings.Factories} onPropertyChange={props.onSelectChange} />
+                            <ESPCheckbox name='MultipleJobs' label='Enable Multiple Jobs' checked={props.adminSettings.MultipleJobs} onPropertyChange={props.onPropertyChange} />
                             <ESPTextField name='IncrementPercentage' label='Increment' helptext='Percentage (%) increment' type='number' onPropertyChange={props.onPropertyChange} value={props.adminSettings.IncrementPercentage} />
-                            <ESPSelect name='ShowingCode' label='Show Quote Number' helptext='Show V6 Quote Number/ESP Code on Job' items={props.adminSettings.ShowingCode}  onPropertyChange={props.onSelectChange} />
+                            <ESPSelect name='ShowingCode' label='Show Quote Number' helptext='Show V6 Quote Number/ESP Code on Job' items={props.adminSettings.ShowingCode} onPropertyChange={props.onSelectChange} />
 
                             <Grid container >
                                 <Grid item lg={3}   >
-                                    <ESPCheckbox name='TrackLateLogin' label='Track late login' checked={props.adminSettings.TrackLateLogin}  onPropertyChange={props.onPropertyChange} />
+                                    <ESPCheckbox name='TrackLateLogin' label='Track late login' checked={props.adminSettings.TrackLateLogin} onPropertyChange={props.onPropertyChange} />
                                 </Grid>
                                 <Grid item lg={9}   >
-                                    <ESPTextField name='LateAllowance' label='Late Login Min' helptext='Number of minutes allowed for late login' type='number' value={props.adminSettings.LateAllowance}  onPropertyChange={props.onPropertyChange} />
+                                    <ESPTextField name='LateAllowance' label='Late Login Min' helptext='Number of minutes allowed for late login' type='number' value={props.adminSettings.LateAllowance} onPropertyChange={props.onPropertyChange} />
                                 </Grid>
                             </Grid>
-                            <ESPCheckbox name='HidePreProductionJobs' label='Hide Pre-production jobs at job claim step' checked={props.adminSettings.HidePreProductionJobs}  onPropertyChange={props.onPropertyChange} />
+                            <ESPCheckbox name='HidePreProductionJobs' label='Hide Pre-production jobs at job claim step' checked={props.adminSettings.HidePreProductionJobs} onPropertyChange={props.onPropertyChange} />
 
                         </Grid>
 
                         <Grid item lg={6}>
-                            <ESPSelect name='Groups' label='Default Groups' helptext='Select Groups for claim' items={props.adminSettings.Groups}  onPropertyChange={props.onSelectChange}  />
-                            <ESPCheckbox name='ScheduledJobs' label='Show Scheduled Jobs After Claim' checked={props.adminSettings.ScheduledJobs}  onPropertyChange={props.onPropertyChange} />
-                            <ESPSelect name='JobListOrdering' label='Job List Order' helptext='Order Showed in Job List Page' items={props.adminSettings.JobListOrdering}  onPropertyChange={props.onSelectChange} />
-                            <ESPCheckbox name='CanFinishWholeJob' label='Enable Finish Whole Job feature' checked={props.adminSettings.CanFinishWholeJob}  onPropertyChange={props.onPropertyChange} />
-                            <ESPCheckbox name='AllowClaimingOutOfFactoryWork' label='Allow claiming on Out of Factory work' checked={props.adminSettings.AllowClaimingOutOfFactoryWork}  onPropertyChange={props.onPropertyChange} />
+                            <ESPSelect name='Groups' label='Default Groups' helptext='Select Groups for claim' items={props.adminSettings.Groups} onPropertyChange={props.onSelectChange} />
+                            <ESPCheckbox name='ScheduledJobs' label='Show Scheduled Jobs After Claim' checked={props.adminSettings.ScheduledJobs} onPropertyChange={props.onPropertyChange} />
+                            <ESPSelect name='JobListOrdering' label='Job List Order' helptext='Order Showed in Job List Page' items={props.adminSettings.JobListOrdering} onPropertyChange={props.onSelectChange} />
+                            <ESPCheckbox name='CanFinishWholeJob' label='Enable Finish Whole Job feature' checked={props.adminSettings.CanFinishWholeJob} onPropertyChange={props.onPropertyChange} />
+                            <ESPCheckbox name='AllowClaimingOutOfFactoryWork' label='Allow claiming on Out of Factory work' checked={props.adminSettings.AllowClaimingOutOfFactoryWork} onPropertyChange={props.onPropertyChange} />
 
                             <Grid container >
                                 <Grid item lg={3}   >
-                                    <ESPCheckbox name='TrackEarlyLeave' label='Track early leave' checked={props.adminSettings.TrackEarlyLeave}  onPropertyChange={props.onPropertyChange} />
+                                    <ESPCheckbox name='TrackEarlyLeave' label='Track early leave' checked={props.adminSettings.TrackEarlyLeave} onPropertyChange={props.onPropertyChange} />
                                 </Grid>
                                 <Grid item lg={9}   >
-                                    <ESPTextField name='EarlyLeaveAlloance' label='Early Leave Min' helptext='Number of minutes allowed for early leave' type='number' value={props.adminSettings.EarlyLeaveAlloance}  onPropertyChange={props.onPropertyChange} />
+                                    <ESPTextField name='EarlyLeaveAlloance' label='Early Leave Min' helptext='Number of minutes allowed for early leave' type='number' value={props.adminSettings.EarlyLeaveAlloance} onPropertyChange={props.onPropertyChange} />
                                 </Grid>
                             </Grid>
                         </Grid>
@@ -172,17 +177,17 @@ const AdminSettingsPage = (props) => {
             </Card>
             <hr />
             <Card >
-                <CardHeader title="Production Board" subheader="Production Board Section Settings" avatar={<Avatar aria-label="recipe" style={{ backgroundColor: grey[900] }}> P </Avatar>
+                <CardHeader title="Production Board" subheader="Production Board Section Settings" avatar={<Avatar aria-label="recipe" style={{ backgroundColor: 'blue' }}> P </Avatar>
                 } />
                 <CardContent>
                     <Grid container spacing={3}>
                         <Grid item lg={6}   >
                             <Grid container>
                                 <Grid item lg={6}   >
-                                    <ESPCheckbox name='PBShowProductionBoard' label='Enable Production Board' checked={props.adminSettings.PBShowProductionBoard}  onPropertyChange={props.onPropertyChange} />
+                                    <ESPCheckbox name='PBShowProductionBoard' label='Enable Production Board' checked={props.adminSettings.PBShowProductionBoard} onPropertyChange={props.onPropertyChange} />
                                 </Grid>
                                 <Grid item lg={6}   >
-                                    <ESPCheckbox name='PBHideOutOfFactoryWorkTypes' label='Hide Out of Factory WorkTypes' checked={props.adminSettings.PBHideOutOfFactoryWorkTypes}  onPropertyChange={props.onPropertyChange} />
+                                    <ESPCheckbox name='PBHideOutOfFactoryWorkTypes' label='Hide Out of Factory WorkTypes' checked={props.adminSettings.PBHideOutOfFactoryWorkTypes} onPropertyChange={props.onPropertyChange} />
                                 </Grid>
                             </Grid>
 
@@ -191,19 +196,19 @@ const AdminSettingsPage = (props) => {
                                     <label >Extra Columns</label>
                                 </Grid>
                                 <Grid item lg={4}>
-                                    <ESPCheckbox name='PBTitleColumn' label='Title' checked={props.adminSettings.PBTitleColumn}  onPropertyChange={props.onPropertyChange} />
+                                    <ESPCheckbox name='PBTitleColumn' label='Title' checked={props.adminSettings.PBTitleColumn} onPropertyChange={props.onPropertyChange} />
                                 </Grid>
                                 <Grid item lg={4}>
-                                    <ESPCheckbox name='PBHourColumn' label='Hours' checked={props.adminSettings.PBHourColumn}  onPropertyChange={props.onPropertyChange} />
+                                    <ESPCheckbox name='PBHourColumn' label='Hours' checked={props.adminSettings.PBHourColumn} onPropertyChange={props.onPropertyChange} />
                                 </Grid>
                                 <Grid item lg={4}>
-                                    <ESPCheckbox name='PBDueDateColumn' label='Due Date' checked={props.adminSettings.PBDueDateColumn}  onPropertyChange={props.onPropertyChange} />
+                                    <ESPCheckbox name='PBDueDateColumn' label='Due Date' checked={props.adminSettings.PBDueDateColumn} onPropertyChange={props.onPropertyChange} />
                                 </Grid>
                             </Grid>
                         </Grid>
 
                         <Grid item lg={6}>
-                            <ESPSelect name='PBJobOrders' label='Job Order' helptext='Listing Job Order' items={props.adminSettings.PBJobOrders}  onPropertyChange={props.onSelectChange} />
+                            <ESPSelect name='PBJobOrders' label='Job Order' helptext='Listing Job Order' items={props.adminSettings.PBJobOrders} onPropertyChange={props.onSelectChange} />
                         </Grid>
                     </Grid>
 
@@ -211,38 +216,37 @@ const AdminSettingsPage = (props) => {
             </Card>
             <hr />
             <Card >
-                <CardHeader title="Performance Statistics" subheader="Performance Statistics Section Settings" avatar={<Avatar aria-label="recipe" style={{ backgroundColor: grey[900] }}> S </Avatar>
+                <CardHeader title="Performance Statistics" subheader="Performance Statistics Section Settings" avatar={<Avatar aria-label="recipe" style={{ backgroundColor: 'blue'  }}> S </Avatar>
                 } />
                 <CardContent>
                     <Grid container spacing={3}>
                         <Grid item lg={6}>
-                            <ESPCheckbox name='PSShowPerformanceStates' label='Enable Performance Stats' checked={props.adminSettings.PSShowPerformanceStates}  onPropertyChange={props.onPropertyChange} />
-                            <ESPSelect name='PSStatisticChart' label='Statistic Chart Type' helptext='' items={props.adminSettings.PSStatisticChart}  onPropertyChange={props.onSelectChange} />
+                            <ESPCheckbox name='PSShowPerformanceStates' label='Enable Performance Stats' checked={props.adminSettings.PSShowPerformanceStates} onPropertyChange={props.onPropertyChange} />
+                            <ESPSelect name='PSStatisticChart' label='Statistic Chart Type' helptext='' items={props.adminSettings.PSStatisticChart} onPropertyChange={props.onSelectChange} />
                         </Grid>
 
                         <Grid item lg={6}>
-                            <ESPCheckbox name='PSEnableIndividual' label='Enable Individual Performance Stats' checked={props.adminSettings.PSEnableIndividual}  onPropertyChange={props.onPropertyChange} />
-                            <ESPSelect name='PSStatisticGroup' label='Statistic Group' helptext='' items={props.adminSettings.PSStatisticGroup}  onPropertyChange={props.onSelectChange} />
+                            <ESPCheckbox name='PSEnableIndividual' label='Enable Individual Performance Stats' checked={props.adminSettings.PSEnableIndividual} onPropertyChange={props.onPropertyChange} />
+                            <ESPSelect name='PSStatisticGroup' label='Statistic Group' helptext='' items={props.adminSettings.PSStatisticGroup} onPropertyChange={props.onSelectChange} />
                         </Grid>
-                        <Grid item lg={11}>
-
+                        <Grid item lg={6}>
+                            <ESPSelect name='PSPerformanceChart' label='Performance Chart Duration' helptext='' items={props.adminSettings.PSPerformanceChart} onPropertyChange={props.onSelectChange} />
                         </Grid>
-                        <Grid item lg={1}>
-                        <Button variant="contained"
-                color="primary"
-                size="large"
-                startIcon={<SaveIcon />}
-                >Save</Button>
+                        <Grid item lg={5}></Grid>
+                        <Grid item lg={1} mx="auto" bgcolor="background.paper">
+                            <Button variant="contained"
+                                color="primary"
+                                className={classes.saveBtn}
+                                size="large"
+                                onClick={props.onSave}
+                                startIcon={<SaveIcon />}
+                            >Save</Button>
                         </Grid>
                     </Grid>
-
                 </CardContent>
             </Card>
-
         </div>
     );
-
-
 };
 
 export default AdminSettingsPage;
