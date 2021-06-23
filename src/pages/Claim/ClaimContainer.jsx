fb34d7cee48cce913cf0939d13f9824d18f02188
reza.souri@gmail.com <reza.souri@gmail.com>
2021-06-24 09:58:11 +1200

    before saving claims

--- src/pages/Claim/ClaimContainer.jsx
+++ src/pages/Claim/ClaimContainer.jsx
@@ -3,11 +3,13 @@ import axios from 'axios';
 import Login from './Login';
 import WorkTypes from './WorkTypes';
 import JobItems from './JobItems';
+import Summary from './Summary';
 import Jobs from './Jobs';
 import config from '../../config';
 import Loginlogics from '../../components/logics/Login';
 import ClaimLogic from '../../components/logics/ClaimLogic';
 import Loading from '../loading.js';
+
 
 class CalimContainer extends React.Component {
     constructor() {
@@ -20,11 +22,14 @@ class CalimContainer extends React.Component {
             jobs: [],
             mainJobs: [],
             claimingOId: 0,
+            claimingUser:'',
             page: 0,
             loading: false,
             worktypeId: 0,
+            jobId:0,
             jobItems: [],
-            mainJobItems: []
+            mainJobItems: [],
+            changedClaimingItems:[]
         };
     }
 
@@ -34,6 +39,7 @@ class CalimContainer extends React.Component {
             ...this.state,
             loading: true
         });
+
         Loginlogics.getListOfWorkersFromApi().then((response) =>
             this.setState({
                 ...this.state,
@@ -42,7 +48,7 @@ class CalimContainer extends React.Component {
                 loading: false
             })
         );
-        this.getWorkTypesFromApi();
+        this.props.changeStep(1);
     }
     componentDidMount() {
 
@@ -50,18 +56,13 @@ class CalimContainer extends React.Component {
 
     /* #region  Login Methods */
 
-
     handleLogin = (id) => {
         const workersList = this.state.workersList;
         const worker = workersList.filter(x => x.OId === id)[0];
         if (worker.IsLoggedIn) {
-            const result = Loginlogics.claimUser(worker.OId, this.state.mainWorkTypes, this.state.workersList);
-            this.setState({
-                ...this.state,
-                page: result.page,
-                claimingOId: result.claimingOId,
-                workTypes: result.workTypes
-            });
+            const page = 1;
+
+            this.goToJobsPage(page, id);
         } else {
             const comment = 'NOT impelemented yet';
             var response = Loginlogics.saveLoginInAPI(id, comment);
@@ -75,9 +76,11 @@ class CalimContainer extends React.Component {
                 if (inx !== -1) {
                     workersList[inx] = worker;
                 }
+
                 this.setState({
                     ...this.state,
                     claimingOId: id,
+                    claimingUser:worker.Name,
                     workersList
                 });
             }).catch((err) => {
@@ -85,6 +88,7 @@ class CalimContainer extends React.Component {
             });
 
         }
+        this.props.changeStep(2);
     }
 
     handleLogOut = () => {
@@ -108,10 +112,11 @@ class CalimContainer extends React.Component {
                 page: 0,
                 workersList
             });
+            this.props.changeStep(1);
+            
         }).catch((err) => {
             alert(err);
         });
-
     }
 
     searchNames = (event) => {
@@ -124,60 +129,36 @@ class CalimContainer extends React.Component {
 
     /* #region  WorkType */
 
-    getWorkTypesFromApi = async () => {
-        var response = {};
-        this.setState({
-            ...this.state,
-            loading: true
-        });
-        await axios.get(`${config.apiUrl}/Claim/GetWorkTypes`).then((data) => {
-            response = JSON.parse(data.data);
-            this.setState({
-                ...this.state,
-                workTypes: response,
-                mainWorkTypes: response,
-                loading: false
-
-            });
-        }).catch((err) => {
-
-            alert(`Error in calling ESP API (Get work types)- ${err}`);
-        });
-
-    }
     searchWorkTypes = (event) => {
         var txt = event.target.value;
         var workTypes = event.target.value.length > 0 ?
             this.state.mainWorkTypes.filter(t => t.Name.toLowerCase().includes(txt.toLowerCase())) :
-            this.state.mainWorkTypes
-            ;
+            this.state.mainWorkTypes;
         this.setState({
             ...this.state,
             workTypes
         });
     }
 
-    handleWorkTypeClick = async (id) => {
-        var response = {};
+    handleWorkTypeClick = async (worktypeId) => {
         this.setState({
             ...this.state,
             loading: true
         });
-        try {
-            var data = await axios.get(`${config.apiUrl}/Claim/GetJobs?id=${id}`);
-            response = JSON.parse(data.data);
+        const response = ClaimLogic.getJobItemsFromApi(this.state.jobId, worktypeId);
+        response.then((r) => {
+            var data = JSON.parse(r.data);
             this.setState({
                 ...this.state,
-                jobs: response,
-                mainJobs: response,
-                page: 2,
+                jobItems: data,
+                mainJobItems: data,
+                page: 3,
                 loading: false,
-                worktypeId: id
+                worktypeId: worktypeId,
             });
-        }
-        catch (err) {
-            alert(`Error in calling ESP API (Get Jobs)- ${err}`);
-        }
+        });
+
+        this.props.changeStep(4);
     }
 
     searchJobs = (event) => {
@@ -196,30 +177,56 @@ class CalimContainer extends React.Component {
     /* #endregion */
 
     /*#region Job*/
+    goToJobsPage = (page, claimingOId) => {
 
+        this.setState({
+            ...this.state,
+            loading: true,
+        });
+
+        const response = ClaimLogic.getJobsOfWorkerFromApi(claimingOId);
+        response.then((r) => {
+            const values = JSON.parse(r.data);
+            this.setState({
+                ...this.state,
+                jobs: values.Item1,
+                mainJobs: values.Item1,
+                workTypes:values.Item2,
+                mainWorkTypes:values.Item2,
+                loading: false,
+                claimingOId,
+                page
+            });
+
+        });
+    }
     handleBack = (pageId) => {
         this.setState({
             page: pageId
         });
+        this.props.changeStep(pageId+1);
     }
     handleJobClick = (jobId) => {
-        const worktypeId = this.state.worktypeId;
-        const response = ClaimLogic.getJobItemsFromApi(jobId, worktypeId);
-        response.then((r) => {
-            var data=JSON.parse(r.data);
-            this.setState({
-                ...this.state,
-                jobItems: data,
-                mainJobItems: data,
-                page: 3
-            });
-        }).catch((err) => {
-            alert(`Error in getting Job items information- ${err}`);
+        this.setState({
+            ...this.state,
+            jobId,
+            page: 2
         });
+        this.props.changeStep(3);
     }
     /* #endregion */
 
+    /*#region jobItem*/
 
+    handleSaveClaim=(changedClaimingItems)=>{
+        this.setState({
+            ...this.state,
+            changedClaimingItems,
+            page: 4
+        });
+        this.props.changeStep(5);
+    }
+    /* #endregion */
     render() {
         const renderConditionaly = () => {
 
@@ -228,27 +235,41 @@ class CalimContainer extends React.Component {
             }
             switch (this.state.page) {
                 case 0: {
-                    return <Login items={this.state.workersList} searchNames={this.searchNames} handleLogin={this.handleLogin} />
+                    return <Login items={this.state.workersList}
+                        searchNames={this.searchNames}
+                        handleLogin={this.handleLogin} />
                 }
                 case 1: {
-                    return <WorkTypes claimingOId={this.state.claimingOId}
-                        searchWorkTypes={this.searchWorkTypes}
-                        workTypes={this.state.workTypes}
-                        handleLogOut={this.handleLogOut}
-                        handleWorkTypeClick={this.handleWorkTypeClick} />
-                }
-                case 2: {
                     return <Jobs claimingOId={this.state.claimingOId}
                         searchJobs={this.searchJobs}
                         jobs={this.state.jobs}
                         handleJobClick={this.handleJobClick}
-                        handleBack={this.handleBack} />
+                        handleLogOut={this.handleLogOut} />
                 }
+                case 2: {
+                    return <WorkTypes claimingOId={this.state.claimingOId}
+                        searchWorkTypes={this.searchWorkTypes}
+                        workTypes={this.state.workTypes}
+                        handleBack={this.handleBack}
+                        handleWorkTypeClick={this.handleWorkTypeClick} />
+                }
+
                 case 3: {
                     return <JobItems claimingOId={this.state.claimingOId}
                         searchJobs={this.searchJobs}
                         items={this.state.mainJobItems}
-                        handleBack={this.handleBack} 
+                        handleBack={this.handleBack}
+                        handleSave={this.handleSaveClaim}
+                    />
+                }
+                case 4: {
+                    return <Summary claimingOId={this.state.claimingOId}
+                        claimingName={this.state.mainWorkersList.find(x=>x.OId==this.state.claimingOId).Name}
+                        worktypeName={this.state.mainWorkTypes.find(x=>x.OId==this.state.worktypeId).Name}
+                        jobName={this.state.mainJobs.find(x=>x.OId==this.state.jobId).Title}
+                        claimingItems={this.state.changedClaimingItems}
+                        handleBack={this.handleBack}
+                        handleSave={this.handleSaveClaim}
                     />
                 }
                 default: return (<div></div>)
