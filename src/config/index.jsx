ef1b02395fc03a4330403b4ebf6207509ae58ea3
reza.souri@gmail.com <reza.souri@gmail.com>
2021-08-31 16:29:22 +1200

    api root change

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,7 +1,7 @@
 const address = {
-  apiUrl: "http://localhost:51804/api/",
+  // apiUrl: "http://localhost:51804/api/",
   // apiUrl: "http://172.16.7.146:8080/api/",
-  //  apiUrl: "http://172.16.1.42/api/",
+  apiUrl: "http://172.16.1.42/api/",
 };
 
 export default address;
