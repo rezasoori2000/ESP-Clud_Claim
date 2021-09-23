0935f1989fb2abc5422d33295e3b571b9c6ed1d4
reza.souri@gmail.com <reza.souri@gmail.com>
2021-09-24 11:59:01 +1200

    change form method

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -77,6 +77,7 @@ export default function SignIn(props) {
           className={classes.form}
           noValidate
           onSubmit={handleSignInClicked}
+          method="post"
         >
           <TextField
             variant="outlined"
